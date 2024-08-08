ad4c06921ab252ecefecd62220ee68b2f541c61a
Nobita4424 <147458576+DungHoang120401@users.noreply.github.com>
2024-08-08 23:07:42 +0700

    Update revenuecat.js

--- revenuecat.js
+++ revenuecat.js
@@ -5,7 +5,7 @@
 [rewrite_local]
 
 # ～ RevenueCat
-^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/[^/]+$) url script-response-body https://gist.githubusercontent.com/nobita_01/dbb1695cd96743eef18f3fac5c6fe227/raw/revenuecat.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/[^/]+$) url script-response-body https://raw.githubusercontent.com/DungHoang120401/Nobita/CSA_Module/revenuecat.js
 ^https:\/\/api\.revenuecat\.com\/.+\/(receipts|subscribers) url script-request-header https://raw.githubusercontent.com/nobita_01/Scripts/master/deleteHeader.js
 
 [mitm]
